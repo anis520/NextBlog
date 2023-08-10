f3e6b531ba3d775d39135988b541f1392fc31bc0
anis520 <anisulhoque587@gmail.com>
2023-08-10 18:00:50 +0600

    verstion 01.1

--- src/app/service/userService.js
+++ src/app/service/userService.js
@@ -3,19 +3,19 @@ import { httpAxios } from "../helper/HttpHelper";
 
 export async function Singup(user) {
   const result = await axios
-    .post("http://localhost:3000/api/user", user)
+    .post("https://devs-school.vercel.app/api/user", user)
     .then((res) => res.data);
   return result;
 }
 export async function login(user) {
   const result = await axios
-    .post("http://localhost:3000/api/login", user)
+    .post("https://devs-school.vercel.app/api/login", user)
     .then((res) => res.data);
   return result;
 }
 export async function logout() {
   const result = await axios
-    .get("http://localhost:3000/api/logout")
+    .get("https://devs-school.vercel.app/api/logout")
     .then((res) => res.data);
   return result;
 }
