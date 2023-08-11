92e4bbe35fac883d3aab4394011d1a12ed49798c
anis520 <anisulhoque587@gmail.com>
2023-08-11 13:58:00 +0600

    fix logout problem 1

--- src/app/service/userService.js
+++ src/app/service/userService.js
@@ -3,29 +3,29 @@ import { httpAxios } from "../helper/HttpHelper";
 
 export async function Singup(user) {
   const result = await axios
-    // .post("https://devs-school.vercel.app/api/user", user)
+    .post("https://devs-school.vercel.app/api/user", user)
     .post("http://localhost:3000/api/user", user)
     .then((res) => res.data);
   return result;
 }
 export async function login(user) {
   const result = await axios
-    .post("http://localhost:3000/api/login", user)
-    // .post("https://devs-school.vercel.app/api/login", user)
+    .post("https://devs-school.vercel.app/api/login", user)
+    // .post("http://localhost:3000/api/login", user)
     .then((res) => res.data);
   return result;
 }
 export async function logout() {
   const result = await axios
+    .get("https://devs-school.vercel.app/api/logout")
     .get("http://localhost:3000/api/logout")
-    // .get("https://devs-school.vercel.app/api/logout")
     .then((res) => res.data);
   return result;
 }
 export async function me() {
   const result = await axios
+    .get("https://devs-school.vercel.app/api/logout")
     .get("http://localhost:3000/api/me")
-    // .get("https://devs-school.vercel.app/api/logout")
     .then((res) => res.data);
   return result;
 }
